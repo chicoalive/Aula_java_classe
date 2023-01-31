b894f803124dbb68bd85ffe56a5f3b0473f5b8f8
chicoalive <chicoalive@gmail.com>
2023-01-31 19:31:46 -0300

    Continuando o loop para alugar quartos

--- src/aplicação/Programa.java
+++ src/aplicação/Programa.java
@@ -40,7 +40,31 @@ public class Programa {
         System.out.println("Quartos disponiveis");
         for (int i = 0; i < 10; i++) {
             if (vetor[i] == null) {
-                System.out.println("Quarto: "+i + ": " + " Disponivel");
+                System.out.println("Quarto: " + i + ": " + " Disponivel");
+            }
+        }
+        System.out.println("");
+        System.out.println("Deseja alugar mais quartos? (s/n): ");
+        char resposta = teclado.next().charAt(0);
+        if (resposta == 's') {
+            for (int i = 0; i < tamanho; i++) {
+                System.out.println("");
+                teclado.nextLine();
+                System.out.println("Aluguel #" + (i + 1) + ":");
+                System.out.print("Nome: ");
+                String nome = teclado.nextLine();
+                System.out.print("Email: ");
+                String email = teclado.nextLine();
+                System.out.print("Quarto: ");
+                int numero = teclado.nextInt();
+                vetor[numero] = new QuartoPessoa(nome, email);
+            }
+            System.out.println("");
+            System.out.println("Quartos Alugados: ");
+            for (int i = 0; i < 10; i++) {
+                if (vetor[i] != null) {
+                    System.out.println(i + ": " + vetor[i]);
+                }
             }
         }
 
